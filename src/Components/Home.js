75473efe9dfe7d32bef25a3b503e6b7681319856
Mrinal Gupta <mrinalgupta04072003@gmail.com>
2024-02-12 23:29:33 +0530

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,7 +14,7 @@ import {Education} from '../Constants';
 //     VerticalTimelineElement,
 // } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import {wave, melappy, waveabout1, wavehm1,} from './../assets';
+import {melappy} from './../assets';
 import {aboutSection} from '../Constants';
 // import Footer from './Footer'
 
